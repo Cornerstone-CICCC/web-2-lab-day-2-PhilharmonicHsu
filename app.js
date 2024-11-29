9b1e95c47bc682e386b192d211e6b18c1717d213
Phil <xuyuwei19940909@gmail.com>
2024-11-29 11:56:43 -0800

    orgnize code

--- app.js
+++ app.js
@@ -1,117 +1,46 @@
+const INIT_USER_ID = 1
+
 $(async function() {
-  let currentUserId = 1;
+  setUpButtons(INIT_USER_ID);
+
+  await getUserPage(INIT_USER_ID);
+})
+
+function setUpButtons(userId) {
   const headerButtons = $('header button');
 
-  await getUserPage(currentUserId);
-
-  headerButtons.first().prop('disabled', true);
-  headerButtons.first().addClass('no-hover')
-
   headerButtons.first().on('click', function(e) {
-    if (currentUserId !== 1) {
-      currentUserId -= 1;
-      getUserPage(currentUserId)
+    if (userId !== 1) {
+      userId -= 1;
+      getUserPage(userId)
     }
   })
 
   headerButtons.last().on('click', function(e) {
-    currentUserId += 1;
-    getUserPage(currentUserId)
+    userId += 1;
+    getUserPage(userId)
   })
 
-  $('.posts').on('click', function(e) {
-    $(this).find('ul').slideToggle()
+  $('.posts > h3').on('click', function(e) {
+    $(this).next().slideToggle()
   })
 
-  $('.todos').on('click', function(e) {
-    $(this).find('ul').slideToggle()
+  $('.todos > h3').on('click', function(e) {
+    $(this).next().slideToggle()
   })
-})
+}
 
 async function getUserPage(userId) {
-  const infoImage = $('.info__image')
-  const infoContent = $('.info__content')
-  const postTitle = $('.posts > h3')
-  const posts = $('.posts > ul')
-  const todoTitle = $('.todos > h3')
-  const todos = $('.todos > ul')
   const headerButtons = $('header button');
-  const container = $('main')
 
-  container.fadeOut(100);
   const user = await getUser(1, userId - 1)
 
-  if (user.hasPrev) {
-    headerButtons.first().prop('disabled', false);
-    headerButtons.first().removeClass('no-hover')
-  } else {
-    headerButtons.first().prop('disabled', true);
-    headerButtons.first().addClass('no-hover')
-  }
+  user.hasPrev ? ableDOM(headerButtons.first()) : disableDOM(headerButtons.first());
+  user.hasNext ? ableDOM(headerButtons.last()) : disableDOM(headerButtons.last());
 
-  if (user.hasNext) {
-    headerButtons.last().prop('disabled', false);
-    headerButtons.last().removeClass('no-hover')
-  } else {
-    headerButtons.last().prop('disabled', true);
-    headerButtons.last().addClass('no-hover')
-  }
-
-  const img = document.createElement('img');
-  img.src = user.user.image;
-  infoImage.empty()
-  infoImage.append(img);
-
-  let contentHTML = `
-    <h2>${user.user.firstName} ${user.user.lastName}</h2>
-    <p><strong>Age: </strong>${user.user.age}</p>
-    <p><strong>Email: </strong>${user.user.email}</p>
-    <p><strong>Phone: </strong>${user.user.phone}</p>`
-
-  infoContent.html(contentHTML)
-
-  const userPosts = await getUserPostsById(user.user.id);
-  postTitle.html(`${user.user.firstName}'s Posts`);
-  let postsInnerHTML = '<li>User has no posts</li>';
-  userPosts.forEach(post => {
-    postsInnerHTML = '';
-    postsInnerHTML += `<li>
-        <h4 class="post-title" data-post-id="${post.id}">${post.title}</h4>
-        <p>${post.body}</p>
-    </li>`
-  })
-  posts.html(postsInnerHTML)
-
-  const userTodos = await getUserTodosById(user.user.id)
-  todoTitle.html(`${user.user.firstName}'s Todos`);
-  let todosInnerHTML = '<li>User has no todos</li>';
-  userTodos.forEach(userTodo => {
-    todosInnerHTML = '';
-    todosInnerHTML += `<li>${userTodo.todo}</li>`
-  })
-  todos.html(todosInnerHTML)
-
-  container.fadeIn('slow');
-
-  $('.post-title').on('click', async function(e) {
-    const postId = $(this).data('postId');
-    const post = await getPostById(postId)
-
-    const dialogHTML = `<dialog id="modal-post-${post.id}" class="modal">
-      <h2>${post.title}</h2>
-      <p>${post.body}</p>
-      <p><em>Views: ${post.views}</em></p>
-      <button id="closeModal">Close Modal</button>
-    </dialog>`
-
-    $('.container').append(dialogHTML)
-
-    document.querySelector(`#modal-post-${post.id}`).showModal();
-
-    $('#closeModal').on('click', function(e) {
-      document.querySelector(`#modal-post-${post.id}`).remove()
-    })
-  })
+  handleInfoSection(user);
+  await handlePastsSection(user)
+  await handleTodosSection(user)
 }
 
 async function getUser(limit, skip= 0) {
@@ -137,16 +66,58 @@ async function getUser(limit, skip= 0) {
   return returnData;
 }
 
-async function getUserInfoById(id) {
-  await $.ajax({
-    url: `https://dummyjson.com/users/${id}`,
-    type: 'GET',
-    success: function(data) {
-      console.log(data)
-    },
-    error: function(error) {
-      console.log(error)
-    }
+function handleInfoSection(user) {
+  const infoImage = $('.info__image')
+  const infoContent = $('.info__content')
+
+  const img = document.createElement('img');
+  img.src = user.user.image;
+  infoImage.empty()
+  infoImage.append(img);
+
+  let contentHTML = `
+    <h2>${user.user.firstName} ${user.user.lastName}</h2>
+    <p><strong>Age: </strong>${user.user.age}</p>
+    <p><strong>Email: </strong>${user.user.email}</p>
+    <p><strong>Phone: </strong>${user.user.phone}</p>`
+
+  infoContent.html(contentHTML)
+}
+
+async function handlePastsSection(user) {
+  const postTitle = $('.posts > h3')
+  const posts = $('.posts > ul')
+
+  const userPosts = await getUserPostsById(user.user.id);
+  postTitle.html(`${user.user.firstName}'s Posts`);
+  let postsInnerHTML = '<li>User has no posts</li>';
+  userPosts.forEach(post => {
+    postsInnerHTML = '';
+    postsInnerHTML += `<li>
+        <h4 class="post-title" data-post-id="${post.id}">${post.title}</h4>
+        <p>${post.body}</p>
+    </li>`
+  })
+  posts.html(postsInnerHTML)
+
+  $('.post-title').on('click', async function(e) {
+    const postId = $(this).data('postId');
+    const post = await getPostById(postId)
+
+    const dialogHTML = `<dialog id="modal-post-${post.id}" class="modal">
+      <h2>${post.title}</h2>
+      <p>${post.body}</p>
+      <p><em>Views: ${post.views}</em></p>
+      <button id="closeModal">Close Modal</button>
+    </dialog>`
+
+    $('.container').append(dialogHTML)
+
+    document.querySelector(`#modal-post-${post.id}`).showModal();
+
+    $('#closeModal').on('click', function(e) {
+      document.querySelector(`#modal-post-${post.id}`).remove()
+    })
   })
 }
 
@@ -165,6 +136,20 @@ async function getUserPostsById(id) {
   })
 
   return posts;
+}
+
+async function handleTodosSection(user) {
+  const todoTitle = $('.todos > h3')
+  const todos = $('.todos > ul')
+
+  const userTodos = await getUserTodosById(user.user.id)
+  todoTitle.html(`${user.user.firstName}'s Todos`);
+  let todosInnerHTML = '<li>User has no todos</li>';
+  userTodos.forEach(userTodo => {
+    todosInnerHTML = '';
+    todosInnerHTML += `<li>${userTodo.todo}</li>`
+  })
+  todos.html(todosInnerHTML)
 }
 
 async function getPostById(postId) {
@@ -199,4 +184,14 @@ async function getUserTodosById(id) {
   })
 
   return todos;
+}
+
+function disableDOM(DOM) {
+  DOM.prop('disabled', true);
+  DOM.addClass('no-hover')
+}
+
+function ableDOM(DOM) {
+  DOM.prop('disabled', false);
+  DOM.removeClass('no-hover')
 }
